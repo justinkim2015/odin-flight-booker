3d5db22f474d25f8bda7517f294d3f2f3bac3a02
justinkim2015 <justinkim2015@gmail.com>
2022-09-01 18:05:00 +0900

    Add information to view/new for bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,6 +1,8 @@
 class BookingsController < ApplicationController
   def new
-    # @booking = Booking.new(bookings_params)
+    @booking = Booking.new
+    @flight = Flight.find(params[:flight_id])
+    @pass_num = params[:number_of_passengers].to_i
   end
 
   def create
